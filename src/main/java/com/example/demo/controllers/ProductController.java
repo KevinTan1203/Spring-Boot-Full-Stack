6dbfa7390e7b45a36ede1ad3f87833fdc038f4a5
Kevin Tan YJ <58412870+KevinTan1203@users.noreply.github.com>
2024-09-13 03:42:11 +0000

    New Commit

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.example.demo.models.Product;
 import com.example.demo.repo.ProductRepo;
 
@@ -13,9 +17,11 @@ public class ProductController {
     private final ProductRepo productRepo;
 
     /*
-     * When springboot creates an instance of the ProductController, it will automatically
-     * create an instance of the ProductRepo and pass it to the new instance of the ProductController.
-    */
+     * When springboot creates an instance of the ProductController, it will
+     * automatically
+     * create an instance of the ProductRepo and pass it to the new instance of the
+     * ProductController.
+     */
     @Autowired
     public ProductController(ProductRepo productRepo) {
         this.productRepo = productRepo;
@@ -28,14 +34,46 @@ public class ProductController {
         return "products/index";
     }
 
-    /*  When we want to add forms, we always need 2 routes
+    /*
+     * When we want to add forms, we always need 2 routes
      * 1 route to display the form
      * 1 route to process the form
-    */
+     */
     @GetMapping("/products/create")
     public String showCreateProductForm(Model model) {
-        var newProduct = new Product();        
+        var newProduct = new Product();
         model.addAttribute("product", newProduct); // Add the instance of the new product to the model
         return "/products/create";
+    }
+
+    @PostMapping("/products/create")
+    public String processCreateProductForm(@ModelAttribute Product newProduct) {
+        // Save the new product to the database
+        productRepo.save(newProduct);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/products/{id}") // URL parameter
+    public String productDetails(@PathVariable Long id, Model model) {
+        // Find the product with matching id
+        var product = productRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Invalid product Id" + id));
+        model.addAttribute("product", product); // Add the product to the view model
+        return "products/details"; // details.html in the products folder
+    }
+
+    @GetMapping("/products/{id}/edit")
+    public String showUpdateProduct(@PathVariable Long id, Model model) {
+        var product = productRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Invalid product Id" + id));
+        model.addAttribute("product", product);
+        return "products/edit";
+    }
+
+    @PostMapping("/products/{id}/edit")
+    public String updateProduct(@PathVariable Long id, @ModelAttribute Product product) {
+        product.setId(id);
+        productRepo.save(product);
+        return "redirect:/products";
     }
 }
